07837d7938dbbe29f7bfe9ed74dd2d3c04915689
jiawei.wang <jiawei.wang@ikang.com>
2017-05-05 17:34:01 +0800

    增加编译器脚本

---
+++ hue/compile/start.sh
@@ -0,0 +1,1 @@
+docker run -v /Users/wjw/Documents/docker/docker_learn/hue/app/hue:/app -v /Users/wjw/Downloads/settings.xml:/root/.m2/settings.xml -v /Users/wjw/.pip:/root/.pip hue-sql-editor make apps
